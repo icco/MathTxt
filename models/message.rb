0b920bebbf61f7a421188519a243b26198ce42c3
Nat Welch <nat@natwelch.com>
2011-10-09 20:45:16 -0700

    Whitespace.

--- models/message.rb
+++ models/message.rb
@@ -1,14 +1,14 @@
 class Message < CouchRest::Model::Base
-   property :text, String
-   property :from, String
-   timestamps!
+  property :text, String
+  property :from, String
+  timestamps!
 
-   ## The first method we call when we get a txt
-   def Message.inc msg, from
-     msg = Message.new
-     msg.text = msg
-     msg.from = from
-     msg.save
+  ## The first method we call when we get a txt
+  def Message.inc msg, from
+    msg = Message.new
+    msg.text = msg
+    msg.from = from
+    msg.save
 
-   end
+  end
 end
