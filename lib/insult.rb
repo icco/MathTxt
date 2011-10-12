35ef054cc35c9622cf721ed2142700ce93382c31
niket <iamniket@gmail.com>
2011-10-12 09:02:40 -0700

    improved insult generator by defining arrays, using 1.9.2 .sample method and adding variables directly in string creation

--- lib/insult.rb
+++ lib/insult.rb
@@ -2,13 +2,30 @@
 # This program is used to generate insults based on two arrays of string inputs.
 
 class Insult
-  @@insultStart = ["Nice hat", "Good haircut", "Nice monicle", "Cool shirt", "Fantastic pants", "Way to show off your talents", "You're not very smart", "Amazing work...kiding"]
-  @@insultEnd = ["jerk-off weirdo", "asshole", "slime-crunch", "bitch-ass", "head-fart", "recalcitrant", "php lover", "ant eating bastard", "sugar coating cream puff", "rainbow seeking troll animal" ]
+  @@insultStart = Array["Nice hat", 
+                            "Good haircut", 
+                            "Nice monicle", 
+                            "Cool shirt", 
+                            "Fantastic pants", 
+                            "Way to show off your talents", 
+                            "You're not very smart", 
+                            "Amazing work...kidding"]
+                            
+  @@insultEnd = Array["jerk-off weirdo", 
+                          "garden snake", 
+                          "slime-crunch", 
+                          "bitch-ass", 
+                          "head-erection", 
+                          "recalcitrant", 
+                          "php lover", 
+                          "ant eating bastard", 
+                          "sugar coated cream puff", 
+                          "rainbow seeking troll" ]
 
   # This will return a string insult generated from two random elements 
   # selected from the class variables @@insultStart and @@insultEnd
   def makeInsult    
-    insult =  "Try this one: " + @@insultStart.choice + ", you " + @@insultEnd.choice
+    insult =  "Try this one: #{@@insultStart.sample}, you #{@@insultEnd.sample}"
     return insult
   end
 end
