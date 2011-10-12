a74fdc92760e552dfd90d6887eaf64a117eeb7e5
niket <iamniket@gmail.com>
2011-10-12 09:17:16 -0700

    renamed makeInsult method to self.makeInsult so it can be used as a class level method

--- lib/insult.rb
+++ lib/insult.rb
@@ -2,29 +2,33 @@
 # This program is used to generate insults based on two arrays of string inputs.
 
 class Insult
-  @@insultStart = Array["Nice hat", 
-                            "Good haircut", 
-                            "Nice monicle", 
-                            "Cool shirt", 
-                            "Fantastic pants", 
-                            "Way to show off your talents", 
-                            "You're not very smart", 
-                            "Amazing work...kidding"]
+  @@insultStart = Array[
+                    "Nice hat", 
+                    "Good haircut", 
+                    "Nice monicle", 
+                    "Cool shirt", 
+                    "Fantastic pants", 
+                    "Way to show off your talents", 
+                    "You're not very smart", 
+                    "Amazing work...kidding"
+                  ]
                             
-  @@insultEnd = Array["jerk-off weirdo", 
-                          "garden snake", 
-                          "slime-crunch", 
-                          "bitch-ass", 
-                          "head-erection", 
-                          "recalcitrant", 
-                          "php lover", 
-                          "ant eating bastard", 
-                          "sugar coated cream puff", 
-                          "rainbow seeking troll" ]
+  @@insultEnd = Array[
+                    "jerk-off weirdo", 
+                    "garden snake", 
+                    "slime-crunch", 
+                    "bitch-ass", 
+                    "head-erection", 
+                    "recalcitrant", 
+                    "php lover", 
+                    "ant eating bastard", 
+                    "sugar coated cream puff", 
+                    "rainbow seeking troll" 
+                  ]
 
   # This will return a string insult generated from two random elements 
   # selected from the class variables @@insultStart and @@insultEnd
-  def makeInsult    
+  def self.makeInsult    
     insult =  "Try this one: #{@@insultStart.sample}, you #{@@insultEnd.sample}"
     return insult
   end
